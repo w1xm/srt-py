Fix axis scaling on npoint scan plot

srt/daemon/daemon.py
@@ -158,7 +158,7 @@ class SmallRadioTelescopeDaemon:
         self.command_error_logs.append((time(), message))
         print(message)
 
-    def n_point_scan(self, object_id):
+    def n_point_scan(self, object_id, grid_size=5):
         """Runs an N-Point (25) Scan About an Object
 
         Parameters
@@ -176,17 +176,17 @@ class SmallRadioTelescopeDaemon:
         cur_vlsr = self.ephemeris_vlsr[object_id]
         self.radio_queue.put(("vlsr", float(cur_vlsr)))
         self.current_vlsr = cur_vlsr
-        N_pnt_default = 25
+        N_pnt_default = grid_size**2
         rotor_loc = []
         pwr_list = []
         #
         scan_center = self.ephemeris_locations[object_id]
-        np_sides = [5, 5]
+        np_sides = [grid_size, grid_size]
         for scan in range(N_pnt_default):
             scan_center = self.ephemeris_locations[object_id] #recompute target position for every iteration
             self.log_message("{0} of {1} point scan.".format(scan, N_pnt_default))
-            i = (scan // 5) - 2
-            j = (scan % 5) - 2
+            i = (scan // grid_size) - int(grid_size/2)
+            j = (scan % grid_size) - int(grid_size/2)
             el_dif = i * self.beamwidth * 0.5
             az_dif_scalar = np.cos((scan_center[1] + el_dif) * np.pi / 180.0)
             # Avoid issues where you get close to the zenith

srt/dashboard/layouts/graphs.py
@@ -416,16 +416,28 @@ def generate_npoint_raw(az_in, el_in, d_az, d_el, pow_in, cent, sides):
     # create the output grid
     az_in = np.array(az_in)
     el_in = np.array(el_in)
-    az_range = np.linspace(az_in.min(), az_in.max(), sides[0])
-    el_range = np.linspace(el_in.min(), el_in.max(), sides[1])
+    
+    idx_center = int(np.ceil(len(az_in)/2))
+    az_center = az_in[idx_center]
+    el_center = el_in[idx_center]
+    
+    #az_span = (sides[0]-1)*d_az
+    #el_span = (sides[1]-1)*d_el
+    
+    az_range = np.linspace(az_center-d_az, az_center+d_az, sides[0])
+    el_range = np.linspace(el_center-d_el, el_center+d_el, sides[1])
+    
+    #below doesn't quite work because we are griddig a moving target
+    #az_range = np.linspace(az_in.min(), az_in.max(), sides[0])
+    #el_range = np.linspace(el_in.min(), el_in.max(), sides[1])
 
     #azout, elout = np.meshgrid(az_a, el_a)
     pow_in = np.array(pow_in)
     pow_grid = np.reshape(pow_in, (sides[0],sides[1]))
     pmin = pow_in.min()
     #p_in = pow_in - pmin
-    x_l = np.linspace(-0.5, 0.5, sides[0])
-    y_l = np.linspace(-0.5, 0.5, sides[1])
+    #x_l = np.linspace(-0.5, 0.5, sides[0])
+    #y_l = np.linspace(-0.5, 0.5, sides[1])
     #xm, ym = np.meshgrid(x_l, y_l)
     #xf = xm.flatten()
     #yf = ym.flatten()