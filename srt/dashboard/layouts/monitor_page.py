c72d64ea1ab2d404aae5082ae87fdd67701f63b5
Daniel Sheen <dsheen@mit.edu>
2024-11-27 16:34:15 -0500

    restructure dashboard power history to not require summing all the spectra at every refresh

--- srt/dashboard/layouts/monitor_page.py
+++ srt/dashboard/layouts/monitor_page.py
@@ -1024,10 +1024,10 @@ def register_callbacks(
         tsys = np.array(status["temp_sys"])
         tcal = np.array(status["temp_cal"])
         cal_pwr = np.array(status["cal_power"])
-        spectrum_history = raw_spectrum_thread.get_history()
-        if spectrum_history is None:
+        power_history = raw_spectrum_thread.get_power_history()
+        if power_history is None:
             return ""
-        return generate_power_history_graph(tsys, tcal, cal_pwr, spectrum_history, num_channels=num_channels)
+        return generate_power_history_graph(tsys, tcal, cal_pwr, power_history, num_channels=num_channels)
 
     @app.callback(
         Output("npoint_info", "data"),
