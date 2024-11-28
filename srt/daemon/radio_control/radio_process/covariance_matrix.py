fixed covariance matrix multiply so it actually works right

srt/daemon/radio_control/radio_process/covariance_matrix.py
@@ -8,21 +8,21 @@ class covariance_matrix_block(gr.sync_block):
     Each output corresponds to one element in the covariance matrix.
     """
 
-    def __init__(self, num_channels=2, num_bins=256):
+    def __init__(self, num_channels=2, vec_length=256):
         """
         Initialize the block.
         Args:
             num_channels (int): Number of input channels.
-            num_bins (int): Number of frequency bins per FFT.
+            vec_length (int): Number of frequency bins per FFT.
         """
         gr.sync_block.__init__(
             self,
             name="Covariance Calculator Block",
-            in_sig=[(np.complex64, num_bins)] * num_channels,
-            out_sig=[(np.complex64, num_bins)] * (num_channels * num_channels),
+            in_sig=[(np.complex64, vec_length)] * num_channels,
+            out_sig=[(np.complex64, vec_length)] * (num_channels**2),
         )
         self.num_channels = num_channels
-        self.num_bins = num_bins
+        self.vec_length = vec_length
 
     def work(self, input_items, output_items):
         """
@@ -30,18 +30,21 @@ class covariance_matrix_block(gr.sync_block):
         """
 
         # Rearrange spectra into a form we can easily handle
-        spectrum_matrices = np.transpose(input_items).reshape((-1, 1, self.num_channels))
+        unwrapped_input = np.array(input_items).reshape(self.num_channels,-1)
 
-        # Get covariance matrices (shape: num_bins, num_channels, num_channels)
+        #combined_data = np.stack(input_items[:self.num_channels], axis=0)
+        spectrum_matrices = np.transpose(unwrapped_input).reshape((-1, 1, self.num_channels))
+
+        # Get covariance matrices (shape: vec_length, num_channels, num_channels)
         covariances = np.matmul(spectrum_matrices.swapaxes(1, 2), spectrum_matrices.conjugate())
 
-        # The shape of covariances is now (num_bins, num_channels, num_channels)
-        # We need to flatten it to the shape (num_channels * num_channels, num_bins)
-        covariances_reshaped = covariances.swapaxes(0, 2).reshape(self.num_channels**2, -1)
+        # The shape of covariances is now (vec_length, num_channels, num_channels)
+        # We need to flatten it to the shape (num_channels * num_channels, vec_length)
+        covariances_reshaped = (covariances.swapaxes(0, 2).reshape(self.num_channels**2, -1, self.vec_length))
 
         # Assign reshaped covariance matrix elements to the output items
         for idx in range(self.num_channels**2):
-            output_items[idx][:] = covariances_reshaped[idx, :]
+            output_items[idx][:] = covariances_reshaped[idx,:]
 
         # Return the number of items produced (number of bins)
         return len(output_items[0])

srt/daemon/radio_control/radio_process/radio_process_dual_channel.py
@@ -187,7 +187,7 @@ class radio_process_dual_channel(gr.top_block):
             num_bins=num_bins,
             num_integrations=num_integrations,
         )
-        self.covariance_matrix_1 = covariance_matrix.covariance_matrix_block(num_channels=num_channels, num_bins=num_bins)
+        self.covariance_matrix_1 = covariance_matrix.covariance_matrix_block(num_channels=num_channels, vec_length=num_bins)
 
         ##################################################
         # Connections
@@ -243,9 +243,9 @@ class radio_process_dual_channel(gr.top_block):
         self.set_fft_window(window.blackmanharris(self.num_bins))
         self.set_sinc_sample_locations(np.arange(-np.pi*4/2.0, np.pi*4/2.0, np.pi/self.num_bins))
         self.set_tag_period(self.num_bins*self.num_integrations)
-        self.covariance_matrix_1.num_bins = self.num_bins
+        self.covariance_matrix_1.vec_length = self.num_bins
         self.weighted_overlap_fft_0.set_num_bins(self.num_bins)
-        self.Wweighted_overlap_fft_0_0.set_num_bins(self.num_bins)
+        self.weighted_overlap_fft_0_0.set_num_bins(self.num_bins)
         self.blocks_tags_strobe_0_0.set_value(pmt.to_pmt({"num_bins": self.num_bins, "samp_rate": self.samp_rate, "num_integrations": self.num_integrations, "motor_az": self.motor_az, "motor_el": self.motor_el, "freq": self.freq, "tsys": [float(n) for n in self.tsys], "tcal": [float(n) for n in self.tcal], "cal_pwr": [float(n) for n in self.cal_pwr], "vlsr": self.vlsr, "glat": self.glat, "glon": self.glon, "soutrack": self.soutrack, "bsw": self.beam_switch, "cal_on":self.cal_on}))
 
 