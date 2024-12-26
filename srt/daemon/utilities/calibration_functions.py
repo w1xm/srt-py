06050cb677e62726ffb5108782d35f98f9eff2f2
Daniel Sheen <dsheen@mit.edu>
2024-12-25 22:59:31 -0500

    reintroduce cal_powers temporarily to not muss up dashboard

--- srt/daemon/utilities/calibration_functions.py
+++ srt/daemon/utilities/calibration_functions.py
@@ -262,4 +262,6 @@ def calculate_calibration_corrections(ref_file, cal_type, tsys=np.array([300]), 
     else:
         raise ValueError(f"Bad cal_type: {cal_type} is not a recognized calibration type")
 
-    return correction_mat.reshape(num_channels**2,-1)
+        average_gains = 1/np.real(np.mean(correction_mat,axis=2))
+
+    return correction_mat.reshape(num_channels**2,-1) average_gains.reshape(num_channels**2)
