0c5dfc831bf385823f7a6b1d149d4e74669fdf2b
Daniel Sheen <dsheen@mit.edu>
2024-12-26 22:27:25 -0500

    timing bug fix

--- srt/daemon/radio_control/radio_process/tagging_and_synchronous_ctl.py
+++ srt/daemon/radio_control/radio_process/tagging_and_synchronous_ctl.py
@@ -98,7 +98,7 @@ class tagging_and_ctl(gr.sync_block):
                 current_rx_time = float(self.rx_time[0]+self.rx_time[1]) + float(self.offset)/self.samp_rate
 
                 if self.next_cal_time:
-                    if (current_rx_time >= self.next_cal_time): #trigger cal state flag change on correct sample even if multiple cycles ahead
+                    if (current_rx_time > self.next_cal_time): #trigger cal state flag change on correct sample even if multiple cycles ahead
                         self.last_cal_state = self.cal_state
                         self.next_cal_time = None
 
